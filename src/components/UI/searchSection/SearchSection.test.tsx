3ca1417262bea45ff3dadfea3451c3f4494c245d
Stanislav <stanislavstranger@gmail.com>
2023-11-19 23:49:34 +0300

    fix: fix SearchSectionTest

--- src/components/UI/searchSection/SearchSection.test.tsx
+++ src/components/UI/searchSection/SearchSection.test.tsx
@@ -1,32 +1,49 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SearchSection from './SearchSection';
-import { SearchProvider } from '../../../context/SearchContext';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { vi } from 'vitest';
+
+const initialState = {
+  searchReducer: {
+    searchTerm: 'InitialValue',
+  },
+  loadingReducer: {
+    loading: false,
+  },
+};
+
+const mockStore = configureStore([])(initialState);
+
+vi.mock('../../../hooks/redux', () => ({
+  ...vi.importActual('../../../hooks/redux'),
+  useAppSelector: vi.fn((selector) => selector(mockStore.getState())),
+  useAppDispatch: vi.fn(() => vi.fn()),
+}));
 
 describe('9. SearchSection Component', () => {
-  it('saves entered value to local storage on Search button click', () => {
+  it('saves entered value to local storage on Search button click', async () => {
     render(
-      <SearchProvider>
-        <SearchSection onSearch={() => {}} loading={false} />
-      </SearchProvider>
+      <Provider store={mockStore}>
+        <SearchSection onSearch={() => {}} />
+      </Provider>
     );
 
     const searchInput = screen.getByPlaceholderText('Select animal');
     const searchButton = screen.getByText('Search');
 
-    fireEvent.change(searchInput, { target: { value: 'TestAnimal' } });
+    fireEvent.change(searchInput, { target: { value: 'InitialValue' } });
     fireEvent.click(searchButton);
 
-    expect(localStorage.getItem('searchTerm')).toBe('TestAnimal');
+    expect(localStorage.getItem('searchTerm')).toBe('InitialValue');
   });
 
   it('retrieves the value from local storage upon mounting', () => {
-    localStorage.setItem('searchTerm', 'InitialValue');
-
     render(
-      <SearchProvider>
-        <SearchSection onSearch={() => {}} loading={false} />
-      </SearchProvider>
+      <Provider store={mockStore}>
+        <SearchSection onSearch={() => {}} />
+      </Provider>
     );
 
     const searchInput = screen.getByPlaceholderText(
